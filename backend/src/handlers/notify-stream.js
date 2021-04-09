f09e16dfca11d0aa534d228ec2bd1690d1ac800c
PALACIOS, YENNY <Yenny.Palacios@Outsourcing.Libertycolombia.com>
2021-04-09 10:29:03 -0500

    feat: change frontend to front

--- backend/src/handlers/notify-stream.js
+++ backend/src/handlers/notify-stream.js
@@ -8,30 +8,42 @@ AWS.config.update({
 const url = 'https://j7fzzhw7xk.execute-api.us-east-1.amazonaws.com/local'
 
 module.exports.main = (event, context, callback) => {
-  try {
-    console.log(JSON.stringify(event, null, 2))
-    event.Records.filter(record => record.eventName === 'MODIFY').forEach(async record => {
-      const connectionId = record.dynamodb.NewImage.connectionId.S
-      const uuid = record.dynamodb.NewImage.uuid.S
-      const status = record.dynamodb.NewImage.status.S
-
-      //Lógica para saber qué mensaje enviar dependiendo del evento del stream
-      const websocketConnector = new AWS.ApiGatewayManagementApi({
-        apiVersion: '2018-11-29',
-        endpoint: url,
-      })
-
-      return await websocketConnector
-        .postToConnection({
-          ConnectionId: connectionId,
-          Data: JSON.stringify(uuid, status),
-        })
-        .promise()
+  console.log(event)
+  const connectionId = event.requestContext.connectionId
+  const websocketConnector = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: url,
+  })
+  return await websocketConnector
+    .postToConnection({
+      ConnectionId: connectionId,
+      Data: JSON.stringify('uuid, status'),
     })
-  } catch (error) {
-    console.log(error)
-    const errorResponse = (0, _requests.makeErrorResponse)('Unexpected error processing the request ' + error.message)
-    callback(JSON.stringify(errorResponse))
-    return errorResponse
-  }
+    .promise()
+  // try {
+  //   console.log(JSON.stringify(event, null, 2))
+  //   event.Records.filter(record => record.eventName === 'MODIFY').forEach(async record => {
+  //     const connectionId = record.dynamodb.NewImage.connectionId.S
+  //     const uuid = record.dynamodb.NewImage.uuid.S
+  //     const status = record.dynamodb.NewImage.status.S
+
+  //     //Lógica para saber qué mensaje enviar dependiendo del evento del stream
+  //     const websocketConnector = new AWS.ApiGatewayManagementApi({
+  //       apiVersion: '2018-11-29',
+  //       endpoint: url,
+  //     })
+
+  //     return await websocketConnector
+  //       .postToConnection({
+  //         ConnectionId: connectionId,
+  //         Data: JSON.stringify(uuid, status),
+  //       })
+  //       .promise()
+  //   })
+  // } catch (error) {
+  //   console.log(error)
+  //   const errorResponse = (0, _requests.makeErrorResponse)('Unexpected error processing the request ' + error.message)
+  //   callback(JSON.stringify(errorResponse))
+  //   return errorResponse
+  // }
 }
---
+++ front/script.js
@@ -0,0 +1,35 @@
+const uuid = 'abc-123'
+const webSocketUrl = `wss://j7fzzhw7xk.execute-api.us-east-1.amazonaws.com/local?uuid=${uuid}`
+
+let webSocket
+
+const onOpen = evt => {
+  console.log('onOpen', evt)
+  alert('WebSocket rocks')
+
+  webSocket.send(JSON.stringify({ uuid }))
+}
+
+const onClose = evt => {
+  console.log('onClose', evt)
+}
+
+const onMessage = evt => {
+  console.log('onMessage', evt)
+  debugger
+  //webSocket.close()
+}
+
+const onError = evt => {
+  console.log('onError', evt)
+  webSocket.close()
+}
+
+const openSocketConnection = () => {
+  webSocket = new WebSocket(webSocketUrl)
+
+  webSocket.onopen = evt => onOpen(evt)
+  webSocket.onclose = evt => onClose(evt)
+  webSocket.onmessage = evt => onMessage(evt)
+  webSocket.onerror = evt => onError(evt)
+}
